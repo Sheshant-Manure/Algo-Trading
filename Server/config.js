02ac62edd06a353467ba76813f49444941296c60
Sheshant-Manure <msheshant1997@gmail.com>
2023-12-25 12:07:05 +0530

    client redux setup

--- Server/config.js
+++ Server/config.js
@@ -1,3 +1,36 @@
+const { FivePaisaClient } = require("5paisajs");
+require('dotenv').config();
+
+// Configuration for your app
+const conf = {
+    appSource: process.env.APPSOURCE,
+    appName: process.env.APPNAME,
+    userId: process.env.USERID,
+    password: process.env.PASSWORD,
+    userKey: process.env.KEY,
+    encryptionKey: process.env.ENCRYPTIONKEY,
+  };
+  
+  var client = new FivePaisaClient(conf);
+  
+  // This client object can be used to login multiple users.
+  client
+   .get_TOTP_Session("53029057", "762792", "879091")
+   .then(response => {
+       const requestToken = response.requestToken;  // Assuming request token is in the response
+       return client.getAccessToken(requestToken);
+    })
+    .then(response => {
+       // Login successful, access token is in response
+       console.log(`Login Successful! Access Token : ${response.accessToken}`);
+       // Proceed with other API calls using the client
+    })
+    .catch(err => {
+       console.log(err);
+    });
+
+
 module.exports = {
     port: 8080,
+    client,
 }
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const app = express()
-const { port } = require('./config')
+const express = require('express');
+const app = express();
+const { port, client } = require('./config');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(client);
 })
+
+app.get('/holdings', (req, res) => {
+  client.getHoldings()
+      .then(holdings => res.send(holdings))
+      .catch(err => res.status(500).send(err));
+});
 
 app.listen(port, () => {
   console.log(`Algo Trading server is live at localhost:${port}`)
